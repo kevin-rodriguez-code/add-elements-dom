4534dbcd4e9173155d6211901ff157d7bb8b9b97
Kevin R. <kevinrc96@hotmail.com>
2023-11-11 11:48:58 +0100

    Add launch.json and change script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
  // Aquí tu código
+const agregar = document.getElementById("agregar")
+const lista = document.getElementById("lista")
+
+agregar.addEventListener("click",function(){
+    const nuevoElemento = prompt("añade un nuevo elemento")
+    const elementoLista = document.createElement ("li")
+    elementoLista.textContent = nuevoElemento
+    lista.appendChild(elementoLista)
+})
